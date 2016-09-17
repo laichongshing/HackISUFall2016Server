fc21df3c381e1d014d72a35f764c8f801e33cbf8
Nischay Venkatram <nischay.v@gmail.com>
2016-09-17 03:16:57 -0500

    Got rid of mongoose packages

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var mongoose = require('mongoose');
 
 var app = express();
 
