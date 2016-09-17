070d5fb7bf67a41428a1dfa3de3c09fb14dbd68e
Jeffrey Haskovec <jeffhaskovec@gmail.com>
2016-09-17 16:48:09 -0500

    Created the meme match function

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Sentencer = require('sentencer');
+var memeMatch = require('memes');
 
 //REST routes
 router.get('/api/captions', function(req, res, next) {
+    // var memes = memeMatch(req.body.tag);
     res.send(Sentencer.make("This test contains {{ a_noun }} and {{ an_adjective }} {{ noun }} in it."));
 });
 
