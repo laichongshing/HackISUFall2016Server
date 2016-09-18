3bf64472d5dc160cc249eadec83f6eb95f4a2531
Nischay Venkatram <nischay.v@gmail.com>
2016-09-18 04:11:14 -0500

    Added meme in tag

--- app/model/Tag.js
+++ app/model/Tag.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var TagSchema = new mongoose.Schema({
     Class: String,
-    prob: Number
+    prob: Number,
+    meme: {type: mongoose.Schema.Types.ObjectId, ref: 'Meme' }
 });
 
 mongoose.model('Tag', TagSchema);
