d80d95d97c890ab46aed5e415b694711b8987c83
Nischay Venkatram <nischay.v@gmail.com>
2016-09-17 19:34:14 -0500

    Pulled out the Clarifai initialization

--- server.js
+++ server.js
@@ -22,10 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
-// app.use(Clarifai.initialize({
-//     'clientId': process.env.CLIENT_ID,
-//     'clientSecret': process.env.CLIENT_SECRET
-// }));
+
+Clarifai.initialize({
+    'clientId': process.env.CLIENT_ID,
+    'clientSecret': process.env.CLIENT_SECRET
+});
 
 // routes
 var routes = require('./app/routes');
